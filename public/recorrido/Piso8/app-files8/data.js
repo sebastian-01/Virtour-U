49cd0fdfd91e931ab9ae7404f311497c5a6fa24d
sebastian <sebasmarin52@gmail.com>
2025-04-21 19:54:24 -0500

    1.0

---
+++ public/recorrido/Piso8/app-files8/data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20250410_104524_00_018",
+      "name": "IMG_20250410_104524_00_018",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.039080188052526665,
+        "pitch": -0.02706735262314197,
+        "fov": 1.3867380618805691
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8327036237371708,
+          "pitch": 0.26050287106233583,
+          "rotation": 10.995574287564278,
+          "target": "1-img_20250410_104627_00_019"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20250410_104627_00_019",
+      "name": "IMG_20250410_104627_00_019",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3797295495265587,
+          "pitch": 0.3196588751735838,
+          "rotation": 0.7853981633974483,
+          "target": "0-img_20250410_104524_00_018"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
